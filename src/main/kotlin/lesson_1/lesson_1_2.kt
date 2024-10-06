1bb5e5acd0ab680e7ceece7a097d24ce7d81b0f1
MaratVa <ItMaratkabest@yandex.ru>
2024-10-06 17:22:28 +0300

    L-1-2 the task is completed

---
+++ src/main/kotlin/lesson_1/lesson_1_2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val sequenceNumber: Int = 75
+    val thankYou:String = "Спасибо за быструю доставку"
+
+    println(sequenceNumber)
+    println(thankYou)
+
+    var numberOfEmployees = 2000
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
