4fe9b6703bcce923fc81ce3b944a0b890c7a79f2
MaratVa <ItMaratkabest@yandex.ru>
2024-10-27 18:08:12 +0300

    L-2-2 the task is completed

---
+++ src/main/kotlin/lesson_2/lesson_2.2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_2
+
+fun main() {
+    val workers = 50
+    val salary1 = 30000
+    val sum1 = workers * salary1
+    println(sum1)
+
+    val intern = 30
+    val salary2 = 20000
+    val sum2 = intern * salary2
+    println(sum1 + sum2)
+
+    println((salary1 + salary2) / 2)
+
+}
