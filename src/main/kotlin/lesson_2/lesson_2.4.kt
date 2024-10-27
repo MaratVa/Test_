f230a26ffddda371e1351816e2148bb63339a529
MaratVa <ItMaratkabest@yandex.ru>
2024-10-27 18:12:40 +0300

    L-2-4 the task is completed

---
+++ src/main/kotlin/lesson_2/lesson_2.4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main () {
+    val crystalOre = 7
+    val ironOre = 11
+
+    val a = crystalOre * 20 / 100
+    println(a)
+
+    val b = ironOre * 20 / 100
+    println(b)
+
+}
