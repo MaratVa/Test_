bef19de7ce0619d5bb1eba5634d3c56b8e61935d
MaratVa <ItMaratkabest@yandex.ru>
2024-10-27 18:15:01 +0300

    L-2-5 the task is completed

---
+++ src/main/kotlin/lesson_2/lesson_2.5.kt
@@ -0,0 +1,66 @@
+package org.example.lesson_2
+
+fun main () {
+    val principalAmount = 70000
+
+    val result1 = principalAmount * (1 + 16.7 / 100)
+    println(result1)
+
+    val result2 = result1 * (1 + 16.7 / 100)
+    println(result2)
+
+    val result3 = result2 * (1 + 16.7 / 100)
+    println(result3)
+
+    val result4 = result3 * (1 + 16.7 / 100)
+    println(result4)
+
+    val result5 = result4 * (1 + 16.7 / 100)
+    println(result5)
+
+    val result6 = result5 * (1 + 16.7 / 100)
+    println(result6)
+
+    val result7 = result6 * (1 + 16.7 / 100)
+    println(result7)
+
+    val result8 = result7 * (1 + 16.7 / 100)
+    println(result8)
+
+    val result9 = result8 * (1 + 16.7 / 100)
+    println(result9)
+
+    val result10 = result9 * (1 + 16.7 / 100)
+    println(result10)
+
+    val result11 = result10 * (1 + 16.7 / 100)
+    println(result11)
+
+    val result12 = result11 * (1 + 16.7 / 100)
+    println(result12)
+
+    val result13 = result12 * (1 + 16.7 / 100)
+    println(result13)
+
+    val result14 = result13 * (1 + 16.7 / 100)
+    println(result14)
+
+    val result15 = result14 * (1 + 16.7 / 100)
+    println(result15)
+
+    val result16 = result15 * (1 + 16.7 / 100)
+    println(result16)
+
+    val result17 = result16 * (1 + 16.7 / 100)
+    println(result17)
+
+    val result18 = result17 * (1 + 16.7 / 100)
+    println(result18)
+
+    val result19 = result18 * (1 + 16.7 / 100)
+    println(result19)
+
+    val result20 = result19  * (1 + 16.7  / 100)
+    val roundOff = String.format("%.3f", result20)
+    println(roundOff)
+}
