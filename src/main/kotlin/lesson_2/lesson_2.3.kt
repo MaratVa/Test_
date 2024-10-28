7ad2bf30f5160246de91536926400bb919fdb0c6
MaratVa <ItMaratkabest@yandex.ru>
2024-10-28 19:53:05 +0300

    L-2-3- train, new version, edits have been made

--- src/main/kotlin/lesson_2/lesson_2.3.kt
+++ src/main/kotlin/lesson_2/lesson_2.3.kt
@@ -1,19 +1,25 @@
 package org.example.lesson_2
 
 fun main() {
-
+    //Зададим время отправления
     val startHours = 9
     val startMinute = 39
 
+    //Зададим время в пути
     val travel = 457
 
-    val start = startHours * 60 + startMinute
+    //Переведем время отправления в минуты
+    val start = startHours * MINUTES_IN_HOUR + startMinute
 
+    //Найдем время прибытия в минуты
     val finish = start + travel
 
-    val finishHours = finish / 60
-    val finishMinutes = finish - finishHours * 60
+    //Переведем время прибытия: в часы и минуты
+    val finishHours = finish / MINUTES_IN_HOUR
+    val finishMinutes = finish % MINUTES_IN_HOUR
 
-    println(finishHours)
-    println(finishMinutes)
+    println(String.format("Время прибытия:%02d:%02d", finishHours, finishMinutes))
+
 }
+
+val MINUTES_IN_HOUR = 60
