39996c0d3b771dfddc9defd45969031def9d044a
MaratVa <ItMaratkabest@yandex.ru>
2024-11-05 20:59:08 +0300

    KS-3-3 the task is completed (multiplication)

---
+++ src/main/kotlin/lesson_3/lesson3_task3.3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+    val number = 8
+    val theMultiplicationTable = """
+        8 * 1 = 8
+        8 * 2 = 16
+        8 * 3 = 24
+        8 * 4 = 32
+        8 * 5 = 40
+        8 * 6 = 48
+        8 * 7 = 56
+        8 * 8 = 64
+        8 * 9 = 72 
+        8 * 10 = 80
+    """.trimIndent()
+    println(theMultiplicationTable)
+}
