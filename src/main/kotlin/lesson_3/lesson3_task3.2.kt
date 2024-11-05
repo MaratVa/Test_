54635a45a207a24f4404e709f5618c60a84d7632
MaratVa <ItMaratkabest@yandex.ru>
2024-11-05 20:53:55 +0300

    KS-3-2 the task is completed

---
+++ src/main/kotlin/lesson_3/lesson3_task3.2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstName = "Андреева Татьяна Сергеевна"
+    val firstAge = "20 лет"
+    val secondName = "Сидорова Татьяна Сергеевна"
+    val secondAge = "22 года"
+
+    println("$firstName, $firstAge")
+    println("$secondName, $secondAge")
+}
