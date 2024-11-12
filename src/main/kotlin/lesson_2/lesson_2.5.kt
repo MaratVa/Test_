2e62799f750cd14ce459bdabaf972d8f358e1dc7
MaratVa <ItMaratkabest@yandex.ru>
2024-11-12 09:23:33 +0300

    KS-2-5 edits

--- src/main/kotlin/lesson_2/lesson_2.5.kt
+++ src/main/kotlin/lesson_2/lesson_2.5.kt
@@ -2,17 +2,15 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+
 fun main() {
     val principalAmount = 70000.0
-    val interestRate = 1.167
+    val interestRatePercentage = 16.7
+    val interestRateDecimal = 1 + interestRatePercentage / 100
 
     val years = 20
 
-    val finalAmount = principalAmount * interestRate.pow(years.toDouble())
+    val finalAmount = principalAmount * interestRateDecimal.pow(years.toDouble())
 
     println("Сумма через $years лет: ${String.format("%.3f", finalAmount)}")
 }
-
-// Масштабируемость: Изменить количество лет очень просто, достаточно изменить значение переменной years.
-// Удобство изменения: процентную ставку также можно изменить, изменив значение переменной interestRate.
-// Чёткость: Код более компактный и легко читается.
