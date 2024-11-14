0eb71367970444dfc59fd0d32a11dfe1f8fc2784
MaratVa <ItMaratkabest@yandex.ru>
2024-11-14 16:13:17 +0300

    KS-7-4 timer

---
+++ src/main/kotlin/lesson_7/lesson7_task.4.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_7
+
+fun main() {
+    print("Введите количество секунд: ")
+    val seconds = readLine()?.toIntOrNull() ?: run {
+        println("Ошибка: Некорректный ввод. Пожалуйста, введите целое число.")
+        return
+    }
+
+    if (seconds <= 0) {
+        println("Ошибка: Введите положительное число секунд.")
+        return
+    }
+
+    val startTime = System.currentTimeMillis()
+    for (i in seconds downTo 1) {
+        println("Осталось секунд: $i")
+        Thread.sleep(1000)
+        if (System.currentTimeMillis() - startTime >= seconds * 1000) {
+            println("Время вышло")
+            return
+        }
+
+    }
+
+    println("Время вышло")
+}
