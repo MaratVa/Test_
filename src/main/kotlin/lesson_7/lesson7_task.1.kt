75117d48b94c1e0725ddc36fee86a745ee61fffb
MaratVa <ItMaratkabest@yandex.ru>
2024-11-15 17:25:43 +0300

    KS-7-1 password

--- src/main/kotlin/lesson_7/lesson7_task.1.kt
+++ src/main/kotlin/lesson_7/lesson7_task.1.kt
@@ -1,19 +1,11 @@
 package org.example.lesson_7
 
-import kotlin.random.Random
-
 fun main() {
-    val chars = ('a'..'z').toList() + ('0'..'9').toList()
     val passwordLength = 6
     val password = buildString {
         for (i in 0 until passwordLength) {
-            val charType = if (i % 2 == 0) 0 else 1
-            val randomIndex = Random.nextInt(if (charType == 0) 26 else 10)
-            if (charType == 0) {
-                append(chars[randomIndex])
-            } else {
-                append(chars[randomIndex + 26])
-            }
+            val char = if (i % 2 == 0) ('a'..'z').random() else ('0'..'9').random()
+            append(char)
         }
     }
     println("Сгенерированный пароль: $password")
