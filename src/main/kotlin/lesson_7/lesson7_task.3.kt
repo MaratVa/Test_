bd86c83ad5ab4476357bcad01c068086f0e5b37d
MaratVa <ItMaratkabest@yandex.ru>
2024-11-15 17:48:53 +0300

    KS-7-3 even numbers new version

--- src/main/kotlin/lesson_7/lesson7_task.3.kt
+++ src/main/kotlin/lesson_7/lesson7_task.3.kt
@@ -1,21 +1,20 @@
 package org.example.lesson_7
 
-import kotlin.random.Random
 
 fun main() {
     print("Введите число: ")
-    val n = readLine()?.toIntOrNull() ?: run {
+    val inputNumber = readLine()?.toIntOrNull() ?: run {
         println("Ошибка: Некорректный ввод. Пожалуйста, введите целое число.")
         return
     }
 
-    if (n < 0) {
+    if (inputNumber < 0) {
         println("Ошибка: Введите неотрицательное число.")
         return
     }
 
-    println("Чётные числа от 0 до $n:")
-    for (i in 0..n step 2) {
+    println("Чётные числа от 0 до $inputNumber:")
+    for (i in 0..inputNumber step 2) {
         println(i)
     }
 }
