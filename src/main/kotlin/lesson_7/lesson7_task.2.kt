076703c0d31bd2406369ad7403d4eb79b6cc1a88
MaratVa <ItMaratkabest@yandex.ru>
2024-11-15 17:41:35 +0300

    KS-7-2 code new version

--- src/main/kotlin/lesson_7/lesson7_task.2.kt
+++ src/main/kotlin/lesson_7/lesson7_task.2.kt
@@ -1,25 +1,19 @@
 package org.example.lesson_7
 
-import kotlin.random.Random
-
 fun main() {
-    val code = Random.nextInt(9000) + 1000
-    println("Ваш код авторизации:$code")
+    do {
+        val code = (1000..9999).random()
+        println("Ваш код авторизации: $code")
 
-    while (true) {
-        print("введите код автоизации:")
+        print("Введите код авторизации: ")
         val inputCode = readLine()?.toIntOrNull()
 
-        if (inputCode != null && inputCode == code) {
-            println("Авторизация успешна!")
-            return
-        } else if (inputCode == null) {
-            println("Ошибка: Неккоректный ввод. Пожалуйста, введите число.")
-        } else {
-            println("Ошибка:Неверный код. Попробуйте снова.")
-            val newCode = Random.nextInt(9000) + 1000
-            println("Ваш код авторизации:$newCode")
-            code == newCode
+        if (inputCode == null) {
+            println("Ошибка: Некорректный ввод. Пожалуйста, введите число.")
+        } else if (inputCode != code) {
+            println("Ошибка: Неверный код. Попробуйте снова.")
         }
-    }
+    } while (inputCode != code)
+
+    println("Авторизация успешна!")
 }
