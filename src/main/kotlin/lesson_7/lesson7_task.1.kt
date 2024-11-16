b835cbed31f1789aa90a39e6521951d230cfd322
MaratVa <ItMaratkabest@yandex.ru>
2024-11-16 19:17:32 +0300

    KS-7-1 password new version

--- src/main/kotlin/lesson_7/lesson7_task.1.kt
+++ src/main/kotlin/lesson_7/lesson7_task.1.kt
@@ -2,9 +2,12 @@ package org.example.lesson_7
 
 fun main() {
     val passwordLength = 6
+    val lowercaseLetters = 'a'..'z'
+    val digits = '0'..'9'
+
     val password = buildString {
         for (i in 0 until passwordLength) {
-            val char = if (i % 2 == 0) ('a'..'z').random() else ('0'..'9').random()
+            val char = if (i % 2 == 0) lowercaseLetters.random() else digits.random()
             append(char)
         }
     }
