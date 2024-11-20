4b2dc834df95c5af6bccd74f889a0025c3ef0930
MaratVa <ItMaratkabest@yandex.ru>
2024-11-20 21:54:48 +0300

    KS-8-2 recipe new version

--- src/main/kotlin/Lesson_8/lesson8_task2.kt
+++ src/main/kotlin/Lesson_8/lesson8_task2.kt
@@ -1,23 +1,16 @@
 package org.example.Lesson_8
 
 fun main() {
-
-    val ingredients = arrayOf("Мука", "Яйца", "Сахар", "Масло", "Молоко")
+    val ingredients = listOf("Мука", "Яйца", "Сахар", "Масло", "Молоко")
 
     print("Введите название ингредиента: ")
-    val ingredientToFind = readLine()!!.toString()
+    val ingredientToFind = readln().lowercase()
 
-    var found = false
-    for (ingredient in ingredients) {
-        if (ingredient == ingredientToFind) {
-            found = true
-            break
-        }
-    }
 
-    if (found) {
+    if (ingredients.any { it.lowercase() == ingredientToFind }) {
         println("Ингредиент $ingredientToFind в рецепте есть")
-    } else {
-        println("Такого ингредиента в рецепте нет")
+        return
     }
+
+    println("Такого ингредиента в рецепте нет")
 }
