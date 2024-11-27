9d9d2b64e6f7ac3faeaac171df36e6ca8a40c685
MaratVa <ItMaratkabest@yandex.ru>
2024-11-27 16:15:07 +0300

    KS-11-3 social networks

---
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -0,0 +1,54 @@
+package org.example.lesson_11
+
+data class VoiceChatUser(
+    val id: Int,
+    val nickname: String,
+    val avatarUrl: String
+)
+
+data class Room(
+    val id: Int,
+    val coverUrl: String,
+    val name: String,
+    val participants: MutableList<VoiceChatUser> = mutableListOf(),
+    val participantStatuses: MutableMap<Int, String> = mutableMapOf()
+) {
+    fun addParticipant(user: VoiceChatUser) {
+        participants.add(user)
+        participantStatuses[user.id] = "микрофон выключен"
+    }
+
+    fun updateParticipantStatus(userId: Int, newStatus: String) {
+        participantStatuses[userId] = newStatus
+    }
+
+    fun printRoomInfo() {
+        println("Название комнаты: $name")
+        println("Обложка: $coverUrl")
+        println("Участники:")
+        participants.forEach { user ->
+            val status = participantStatuses[user.id] ?: "Неизвестно"
+            println("  - ${user.nickname} (${user.avatarUrl}) - $status")
+        }
+    }
+}
+
+fun main() {
+    val user1 = VoiceChatUser(1, "user123", "avatar1.jpg")
+    val user2 = VoiceChatUser(2, "some_user", "avatar2.png")
+    val user3 = VoiceChatUser(3, "anotherOne", "avatar3.jpeg")
+
+    val room = Room(
+        1,
+        "cover.jpg",
+        "Комната для обсуждения Kotlin",
+    )
+
+    room.addParticipant(user1)
+    room.addParticipant(user2)
+    room.addParticipant(user3)
+    room.updateParticipantStatus(user1.id, "разговаривает")
+    room.updateParticipantStatus(user3.id, "пользователь заглушен")
+
+    room.printRoomInfo()
+}
