27b374bcc6fc4ab0f6c1dfc402753b1c1a3a6b38
MaratVa <ItMaratkabest@yandex.ru>
2024-11-28 16:24:45 +0300

    KS-11-1 class new version

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,11 +1,14 @@
 package org.example.lesson_11
 
-data class User(
-    val userId: Int,
-    val login: String,
-    val password: String,
-    val email: String
-)
+class User(
+    private val userId: Int,
+    private val login: String,
+    private val password: String,
+    private val email: String
+) {
+    override fun toString(): String = "User(userId=$userId, password = $password,login=$login, email=$email)"
+
+}
 
 fun main() {
     val user1 = User(1, "johndoe", "password123", "john.doe@example.com")
