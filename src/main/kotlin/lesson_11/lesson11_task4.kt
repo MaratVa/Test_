34145bee4a1c08d4e9e1ebd605febb458196058a
MaratVa <ItMaratkabest@yandex.ru>
2024-11-28 17:57:33 +0300

    KS-11-4 food

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,49 @@
+package org.example.lesson_11
+
+class RecipeCategory(
+    val categoryId: Int,
+    val name: String
+) {
+    override fun toString(): String = "RecipeCategory(categoryId=$categoryId, name='$name')"
+}
+
+class Ingredient(
+    private val ingredientId: Int,
+    val name: String,
+    private val quantity: String = "1",
+    private val unit: String = "unit"
+) {
+    override fun toString(): String =
+        "Ingredient(ingredientId=$ingredientId, name='$name', quantity='$quantity', unit='$unit')"
+}
+
+class Recipe(
+    private val recipeId: Int,
+    val title: String,
+    private val categoryId: Int,
+    val ingredients: List<Ingredient>
+) {
+    override fun toString(): String =
+        "Recipe(recipeId=$recipeId, title='$title', categoryId=$categoryId, ingredients=$ingredients)"
+}
+
+fun main() {
+    val burgerCategory = RecipeCategory(1, "Burgers")
+    val ingredients = listOf(
+        Ingredient(1, "Beef patty"),
+        Ingredient(2, "Mushrooms"),
+        Ingredient(3, "Cheese"),
+        Ingredient(4, "White mushroom sauce"),
+        Ingredient(5, "Red onion"),
+        Ingredient(6, "Tomatoes"),
+        Ingredient(7, "Burger buns", "2", "pieces")
+    )
+    val mushroomCheeseBurger = Recipe(1, "Burger with Mushrooms and Cheese", burgerCategory.categoryId, ingredients)
+
+    println("Category: ${burgerCategory.name}")
+    println("Recipe: ${mushroomCheeseBurger.title}")
+    println("Ingredients:")
+    mushroomCheeseBurger.ingredients.forEach { ingredient ->
+        println("- ${ingredient.name}")
+    }
+}
