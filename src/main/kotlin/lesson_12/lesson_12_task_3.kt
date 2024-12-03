f4a238037b980a6febb146e6153b5813a7d0f63a
MaratVa <ItMaratkabest@yandex.ru>
2024-12-03 20:10:19 +0300

    KS-12-3 WeatherReportInCelsius

--- src/main/kotlin/lesson_12/lesson_12_task_3.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_3.kt
@@ -11,10 +11,10 @@ class WeatherReportInCelsius(
     private val nightTemperatureKelvin: Int = kelvinTemperatureNight
 
     private val dailyTemperatureCelsius: Int
-        get() = dailyTemperatureKelvin - 273
+        get() = dailyTemperatureKelvin - TemperatureConstants.KELVIN_OFFSET
 
     private val nightTemperatureCelsius: Int
-        get() = nightTemperatureKelvin - 273
+        get() = nightTemperatureKelvin - TemperatureConstants.KELVIN_OFFSET
 
     fun displayWeather() {
         val precipitationString = if (precipitation) "Да" else "Нет"
@@ -27,6 +27,13 @@ class WeatherReportInCelsius(
 }
 
 fun main() {
-    val weatherReport = WeatherReportInCelsius(true, 298, 291)
+    val weatherReport =
+        WeatherReportInCelsius(true, TemperatureConstants.DAY_TEMP_KELVIN, TemperatureConstants.NIGHT_TEMP_KELVIN)
     weatherReport.displayWeather()
+}
+
+object TemperatureConstants {
+    const val KELVIN_OFFSET = 273
+    const val DAY_TEMP_KELVIN = 298
+    const val NIGHT_TEMP_KELVIN = 291
 }
