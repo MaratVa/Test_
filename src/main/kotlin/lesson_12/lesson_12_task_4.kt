4394cb7b173a6dc1e8de94d3aa9e2f31d8d30c65
MaratVa <ItMaratkabest@yandex.ru>
2024-12-03 20:22:01 +0300

    KS-12-4 WeatherData

--- src/main/kotlin/lesson_12/lesson_12_task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -2,29 +2,35 @@ package lesson_12
 
 import java.time.LocalDate
 
-class WeatherData(
-    private val dailyTemperatureKelvin: Int,
-    private val nightTemperatureKelvin: Int,
-    private val precipitation: Boolean
-) {
-
-    private val dailyTemperatureCelsius: Int get() = dailyTemperatureKelvin - 273
-    private val nightTemperatureCelsius: Int get() = nightTemperatureKelvin - 273
+class WeatherData(private val precipitation: Boolean) {
+    private var dailyTemperatureKelvin: Int = 0
+    private var nightTemperatureKelvin: Int = 0
 
     init {
+        dailyTemperatureKelvin = TemperatureConstants.DAY_TEMP_KELVIN
+        nightTemperatureKelvin = TemperatureConstants.NIGHT_TEMP_KELVIN
         displayWeather()
     }
+
+    private val dailyTemperatureCelsius: Int get() = dailyTemperatureKelvin - TemperatureConstants.KELVIN_OFFSET
+    private val nightTemperatureCelsius: Int get() = nightTemperatureKelvin - TemperatureConstants.KELVIN_OFFSET
 
     private fun displayWeather() {
         val precipitationString = if (precipitation) "Да" else "Нет"
         println("Дата: ${LocalDate.now()}")
-        println("Дневная температура: ${dailyTemperatureCelsius}°C")
-        println("Ночная температура: ${nightTemperatureCelsius}°C")
+        println("Дневная температура: $dailyTemperatureCelsius°C")
+        println("Ночная температура: $nightTemperatureCelsius°C")
         println("Осадки: $precipitationString")
         println("------------------")
     }
 }
 
 fun main() {
-    WeatherData(298, 273, true)
+    WeatherData(true)
+}
+
+object TemperatureConstants {
+    const val KELVIN_OFFSET = 273
+    const val DAY_TEMP_KELVIN = 298
+    const val NIGHT_TEMP_KELVIN = 273
 }
