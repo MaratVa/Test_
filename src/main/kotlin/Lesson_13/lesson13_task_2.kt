d01e3dbd72fcd1ca79084544bcf5806dcdebff24
MaratVa <ItMaratkabest@yandex.ru>
2024-12-03 21:45:51 +0300

    KS-13-1 telephone2

---
+++ src/main/kotlin/Lesson_13/lesson13_task_2.kt
@@ -0,0 +1,20 @@
+package org.example.Lesson_13
+
+class Contact2(
+    private val name: String,
+    private val phoneNumber: String,
+    private var company: String?
+) {
+
+    fun printContactInfo() {
+        println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: ${company ?: "<не указано>"}")
+    }
+}
+
+fun main() {
+    val contact = Contact2("Ростислав", "89123456789", null)
+    contact.printContactInfo()
+
+    val contact2 = Contact2("Иван", "1234567890", "OpenAI")
+    contact2.printContactInfo()
+}
