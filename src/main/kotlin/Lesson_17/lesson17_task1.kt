2d1527d38d09f3a900e8d4efb2f08f3877c077bf
MaratVa <ItMaratkabest@yandex.ru>
2024-12-12 19:55:48 +0300

    KS-17-1-Victorina

---
+++ src/main/kotlin/Lesson_17/lesson17_task1.kt
@@ -0,0 +1,25 @@
+package org.example.Lesson_17
+
+class QuizItem(
+    private val question: String,
+    private var answer: String
+) {
+
+    val questionText: String
+        get() = question
+
+    var answerText: String
+        get() = answer
+        set(value) {
+            answer = value
+        }
+}
+
+fun main() {
+    val quizItem = QuizItem("Какая планета является самой большой?", "Юпитер")
+    println("Вопрос: ${quizItem.questionText}")
+    println("Ответ: ${quizItem.answerText}")
+
+    quizItem.answerText = "Сатурн"
+    println("Изменённый ответ: ${quizItem.answerText}")
+}
