1f72ed4957688839e7c0bfbe01e81cf24086106d
MaratVa <ItMaratkabest@yandex.ru>
2024-12-13 21:38:58 +0300

    KS-17-2-ship

--- src/main/kotlin/Lesson_17/lesson17_task2.kt
+++ src/main/kotlin/Lesson_17/lesson17_task2.kt
@@ -1,17 +1,32 @@
 package org.example.Lesson_17
 
 class Ship(
-    private val name: String,
-    var averageSpeed: Double,
-    var port: String
+    private var name: String,
+    private var averageSpeed: Double,
+    private var port: String
 ) {
 
-    fun changeName(newName: String) {
-        println("Предупреждение: имя корабля не может быть изменено!")
-    }
+    var shipName: String
+        get() = name
+        set(value) {
+            println("Предупреждение: имя корабля не может быть изменено!")
+            name = value
+        }
+
+    var shipAverageSpeed: Double
+        get() = averageSpeed
+        set(value) {
+            averageSpeed = value
+        }
+
+    var shipPort: String
+        get() = port
+        set(value) {
+            port = value
+        }
 
     fun displayShipInfo() {
-        println("Имя: $name, Средняя скорость: $averageSpeed, Порт приписки: $port")
+        println("Имя: $shipName, Средняя скорость: $shipAverageSpeed, Порт приписки: $shipPort")
     }
 }
 
@@ -19,10 +34,10 @@ fun main() {
     val myShip = Ship("Enterprise", 30.5, "Starbase")
     myShip.displayShipInfo()
 
-    myShip.changeName("USS Constitution")
+    myShip.shipName = "USS Constitution"
     myShip.displayShipInfo()
 
-    myShip.averageSpeed = 35.0
-    myShip.port = "New Earth"
+    myShip.shipAverageSpeed = 35.0
+    myShip.shipPort = "New Earth"
     myShip.displayShipInfo()
 }
