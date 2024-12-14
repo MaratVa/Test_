f5479b206bd7a8924b10e69a264b654c2c9e5af1
MaratVa <ItMaratkabest@yandex.ru>
2024-12-14 21:19:24 +0300

    KS-17-1-victorina

--- src/main/kotlin/Lesson_17/lesson17_task1.kt
+++ src/main/kotlin/Lesson_17/lesson17_task1.kt
@@ -2,23 +2,18 @@ package org.example.Lesson_17
 
 class QuizItem(
     private val question: String,
-    private var answer: String
+    var answer: String
 ) {
     val questionText: String
         get() = question
 
-    var answerText: String
-        get() = this.answer
-        set(value) {
-            this.answer = value
-        }
 }
 
 fun main() {
     val quizItem = QuizItem("Какая планета является самой большой?", "Юпитер")
     println("Вопрос: ${quizItem.questionText}")
-    println("Ответ: ${quizItem.answerText}")
+    println("Ответ: ${quizItem.answer}")
 
-    quizItem.answerText = "Сатурн"
-    println("Изменённый ответ: ${quizItem.answerText}")
+    quizItem.answer = "Сатурн"
+    println("Изменённый ответ: ${quizItem.answer}")
 }
