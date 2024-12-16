a1b92a31dbe60ef41f75a98f9a4c017127e8ca50
MaratVa <ItMaratkabest@yandex.ru>
2024-12-16 20:15:27 +0300

    KS-19-3-ship

---
+++ src/main/kotlin/Lesson_19/Lesson_19_task3.kt
@@ -0,0 +1,31 @@
+package org.example.Lesson_19
+
+import kotlin.NotImplementedError
+
+class SpaceShip {
+    fun takeOff() {
+        println("Корабль взлетает...")
+        //TODO("Здесь должна быть дополнительная логика для взлета")
+    }
+
+    fun land() {
+        println("Корабль приземляется")
+        throw NotImplementedError("Метод приземления не реализован")
+    }
+
+    fun shootAsteroid() {
+        println("Корабль стреляет по астероиду!")
+    }
+}
+
+fun main() {
+    val ship = SpaceShip()
+
+    ship.takeOff()
+    try {
+        ship.land()
+    } catch (e: NotImplementedError) {
+        println("Ошибка: ${e.message}")
+    }
+    ship.shootAsteroid()
+}
