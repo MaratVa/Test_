28dcc1348e63946d8ad9ecad65413ca78568ad29
MaratVa <ItMaratkabest@yandex.ru>
2024-12-19 19:34:22 +0300

    KS-21-3 player

---
+++ src/main/kotlin/Lesson_21/Lesson21_task3.kt
@@ -0,0 +1,26 @@
+package org.example.Lesson_21
+
+class Player(
+    val name: String,
+    var currentHealth: Int,
+    val maxHealth: Int
+)
+
+fun Player.isHealthy(): Boolean {
+    return currentHealth == maxHealth
+}
+
+fun main() {
+    val player1 = Player("Герой", 100, 100)
+    println("${player1.name} здоров: ${player1.isHealthy()}")
+
+    val player2 = Player("Вор", 50, 100)
+    println("${player2.name} здоров: ${player2.isHealthy()}")
+
+    val healPlayer: (Player) -> Unit = { player ->
+        player.currentHealth = player.maxHealth
+    }
+
+    healPlayer(player2)
+    println("${player2.name} здоров после лечения: ${player2.isHealthy()}") // true
+}
