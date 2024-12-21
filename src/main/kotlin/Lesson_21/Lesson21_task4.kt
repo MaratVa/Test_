647862562e9b585cedfcf3b0202c3cf8df41e38b
MaratVa <ItMaratkabest@yandex.ru>
2024-12-21 19:52:23 +0300

    KS-21-4 bot

--- src/main/kotlin/Lesson_21/Lesson21_task4.kt
+++ src/main/kotlin/Lesson_21/Lesson21_task4.kt
@@ -1,29 +1,11 @@
 package org.example.Lesson_21
 
 import java.io.File
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.FileReader
-import java.io.FileWriter
-
 
 fun File.writeWordToFile(word: String) {
     val lowerCaseWord = word.lowercase()
-    val tempFile = File.createTempFile("temp", null)
-
-    BufferedReader(FileReader(this)).use { reader ->
-        BufferedWriter(FileWriter(tempFile)).use { writer ->
-            writer.write("$lowerCaseWord\n")
-            var line: String?
-            while (reader.readLine().also { line = it } != null) {
-                writer.write("$line\n")
-            }
-        }
-    }
-
-    this.delete()
-    tempFile.copyTo(this, overwrite = true)
-    tempFile.delete()
+    val existingContent = if (this.exists()) this.readText() else ""
+    this.writeText("$lowerCaseWord\n$existingContent")
 }
 
 fun main() {
