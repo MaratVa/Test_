6008344b797affafc0096bb2924a1f372e45f3fa
MaratVa <ItMaratkabest@yandex.ru>
2024-12-22 13:50:40 +0300

    KS-22-5 Galactic

---
+++ src/main/kotlin/Lesson_22/Lesson22_task5.kt
@@ -0,0 +1,26 @@
+package org.example.Lesson_22
+
+import java.time.LocalDateTime
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val eventDateTime: LocalDateTime,
+    val distanceFromEarth: Double
+)
+
+fun main() {
+    val alphaCentauri = GalacticGuide(
+        name = "Система Альфа Центавра",
+        description = "Ближайшая к нам звёздная система, состоящая из трёх звёзд: Альфа Центавра A, Альфа Центавра B и Проксима Центавра",
+        eventDateTime = LocalDateTime.of(2024, 1, 1, 0, 0),
+        distanceFromEarth = 4.37
+    )
+
+    val (name, description, dateTime, distance) = alphaCentauri
+
+    println("Название: $name")
+    println("Описание: $description")
+    println("Дата и время: $dateTime")
+    println("Расстояние от Земли: $distance световых лет")
+}
