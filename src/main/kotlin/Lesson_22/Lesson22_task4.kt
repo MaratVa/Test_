2d0ab1792a447b7c77e44f036976e9d8044859ee
MaratVa <ItMaratkabest@yandex.ru>
2024-12-26 20:06:37 +0300

    KS-22-4 Screen

--- src/main/kotlin/Lesson_22/Lesson22_task4.kt
+++ src/main/kotlin/Lesson_22/Lesson22_task4.kt
@@ -1,26 +1,48 @@
 package org.example.Lesson_22
 
-data class MainScreenState(
-    val data: String = "",
-    val isLoading: Boolean = false
-)
-
 class MainScreenViewModel {
-    var mainScreenState = MainScreenState()
+
+    private var _mainScreenState = MainScreenState()
+
+    val mainScreenState: MainScreenState
+        get() = _mainScreenState
+
+    private val listeners = mutableListOf<(MainScreenState) -> Unit>()
+
+    fun subscribe(listener: (MainScreenState) -> Unit) {
+        listeners.add(listener)
+    }
+
+    private fun notifySubscribers() {
+        listeners.forEach { it(_mainScreenState) }
+    }
 
     fun loadData() {
-        mainScreenState = mainScreenState.copy(isLoading = false, data = "")
-        println("Состояние: ${mainScreenState}")
+        _mainScreenState = MainScreenState(isLoading = false, data = "")
+        notifySubscribers()
+        println("Состояние 1: $_mainScreenState")
 
-        mainScreenState = mainScreenState.copy(isLoading = true)
-        println("Состояние: ${mainScreenState}")
+        _mainScreenState = _mainScreenState.copy(isLoading = true)
+        notifySubscribers()
+        println("Состояние 2: ${_mainScreenState}")
 
-        mainScreenState = mainScreenState.copy(isLoading = false, data = "Данные загружены!")
-        println("Состояние: ${mainScreenState}")
+        _mainScreenState = _mainScreenState.copy(isLoading = false, data = "Данные загружены!")
+        notifySubscribers()
+        println("Состояние 3: $_mainScreenState")
     }
+
+    data class MainScreenState(
+        val data: String = "",
+        val isLoading: Boolean = false
+    )
 }
 
 fun main() {
     val viewModel = MainScreenViewModel()
+
+    viewModel.subscribe { state ->
+        println("UI получил изменение состояния: $state")
+    }
+
     viewModel.loadData()
 }
